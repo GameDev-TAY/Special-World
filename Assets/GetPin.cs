d2de967a04c4a145f0a48b08062e6dc6fa6c64fa
Ariel Duek <arielduek@gmail.com>
2020-12-27 22:14:16 +0200

    first room updated

---
+++ Assets/GetPin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPin : MonoBehaviour
+{
+    [SerializeField] GameObject Box;
+    private AudioSource foundBox;
+    public bool onPin; //Change to private
+
+    void OnTriggerEnter(Collider other)
+    {
+        onPin = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onPin = false;
+    }
+
+    void Start()
+    {
+        foundBox = Box.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onPin)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                this.gameObject.SetActive(false);
+                Box.GetComponent<LightSwitch>().hasPin = true;
+                Box.GetComponent<LightSwitch>().LastSwitch = this.gameObject;
+                foundBox.Play();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (onPin)
+        {
+            GUI.Box(new Rect(0, 0, 200, 20), "Press E to Get the switch");
+        }
+    }
+}
---
+++ Assets/Scripts/4-Light/LightSwitch.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightSwitch : MonoBehaviour
+{
+
+    [SerializeField] GameObject Light;
+    [SerializeField] GameObject trigger;
+    [SerializeField] GameObject CorrectSwitch;
+    public GameObject LastSwitch;
+    private AudioSource Click;
+    public bool onSwitch; //Change to private
+    public bool hasPin = false;
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (hasPin && LastSwitch == CorrectSwitch) {
+            trigger.SetActive(true);
+            onSwitch = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        onSwitch = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Click = trigger.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onSwitch)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Light.GetComponent<Light>().enabled = !Light.GetComponent<Light>().enabled;
+                trigger.GetComponent<Transform>().Rotate(0,0,90f);
+                Click.Play();
+                this.GetComponent<AudioSource>().Stop();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (onSwitch)
+        {
+            if (Light.GetComponent<Light>().enabled)
+            {
+                GUI.Box(new Rect(0, 0, 200, 20), "Press E to close the light");
+            }
+            else
+            {
+                GUI.Box(new Rect(0, 0, 200, 20), "Press E to open the light");
+            }
+        }
+    }
+}
